d6cbc3feb25c3656e272cd730b9f159015c575d3
AmrRaafat <amrraafat>
2026-09-08 17:25:39 +0300

    Add project files.

---
+++ G-ASP-NET-101-C#Basics-04/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace G_ASP_NET_101_C_Basics_04
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            #region Question 1
+
+            // Create a one-dimensional array of double values
+            double[] prices = { 25.5, 40.0, 33.75 };
+
+            // Print the second price (index 1)
+            Console.WriteLine(prices[1]);
+
+            #endregion
+        }
+    }
+}
